5616814f896b099fe4195e0f5cb52c253f6fe469
Dulz97 <70124334+Dulz97@users.noreply.github.com>
2020-10-30 21:01:52 +0530

    Update juice.java

--- app/src/main/java/com/example/myapplication/juice.java
+++ app/src/main/java/com/example/myapplication/juice.java
@@ -1,5 +1,5 @@
 package com.example.myapplication;
-sd
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
