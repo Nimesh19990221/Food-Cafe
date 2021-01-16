f8b6f0a1f696ed3d6fda4049f873d2da20071c5a
Navodyadesilva <71263012+Navodyadesilva@users.noreply.github.com>
2021-01-16 21:36:42 +0530

    Update scroll2.java

--- app/src/main/java/com/example/myapplication/scroll2.java
+++ app/src/main/java/com/example/myapplication/scroll2.java
@@ -4,25 +4,162 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
 public class scroll2 extends AppCompatActivity {
     Button bur1;
     Button bur2;
     Button bur3;
     Button bur4;
-
+    Button download,download1,download2,download3;
+    ImageView image1,image2,image3,image4;
+    OutputStream outputStream;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scroll2);
+        download=findViewById(R.id.download);
+        download1=findViewById(R.id.download1);
+        download2=findViewById(R.id.download2);
+        download3=findViewById(R.id.download3);
+        image1=findViewById(R.id.image1);
+        image2=findViewById(R.id.image2);
+        image3=findViewById(R.id.image3);
+        image4=findViewById(R.id.image4);
+        download.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BitmapDrawable drawable=(BitmapDrawable)image1.getDrawable();
+                Bitmap bitmap=drawable.getBitmap();
+
+                File filepath= Environment.getExternalStorageDirectory();
+                File dir=new File(filepath.getAbsolutePath()+"/Demo/");
+                dir.mkdir();
+                File file=new File(dir, System.currentTimeMillis()+".jpg");
+                try {
+                    outputStream=new FileOutputStream(file);
+                }catch (FileNotFoundException e){
+                    e.printStackTrace();
+                }
+                bitmap.compress(Bitmap.CompressFormat.JPEG,100,outputStream);
+                Toast.makeText(getApplicationContext(),"Image Download to Internal !!!",Toast.LENGTH_SHORT).show();
+                try {
+                    outputStream.flush();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    outputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        download1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BitmapDrawable drawable=(BitmapDrawable)image2.getDrawable();
+                Bitmap bitmap=drawable.getBitmap();
+
+                File filepath= Environment.getExternalStorageDirectory();
+                File dir=new File(filepath.getAbsolutePath()+"/Demo/");
+                dir.mkdir();
+                File file=new File(dir, System.currentTimeMillis()+".jpg");
+                try {
+                    outputStream=new FileOutputStream(file);
+                }catch (FileNotFoundException e){
+                    e.printStackTrace();
+                }
+                bitmap.compress(Bitmap.CompressFormat.JPEG,100,outputStream);
+                Toast.makeText(getApplicationContext(),"Image Download to Internal !!!",Toast.LENGTH_SHORT).show();
+                try {
+                    outputStream.flush();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    outputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        download2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BitmapDrawable drawable=(BitmapDrawable)image3.getDrawable();
+                Bitmap bitmap=drawable.getBitmap();
+
+                File filepath= Environment.getExternalStorageDirectory();
+                File dir=new File(filepath.getAbsolutePath()+"/Demo/");
+                dir.mkdir();
+                File file=new File(dir, System.currentTimeMillis()+".jpg");
+                try {
+                    outputStream=new FileOutputStream(file);
+                }catch (FileNotFoundException e){
+                    e.printStackTrace();
+                }
+                bitmap.compress(Bitmap.CompressFormat.JPEG,100,outputStream);
+                Toast.makeText(getApplicationContext(),"Image Download to Internal !!!",Toast.LENGTH_SHORT).show();
+                try {
+                    outputStream.flush();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    outputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        download3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BitmapDrawable drawable=(BitmapDrawable)image4.getDrawable();
+                Bitmap bitmap=drawable.getBitmap();
+
+                File filepath= Environment.getExternalStorageDirectory();
+                File dir=new File(filepath.getAbsolutePath()+"/Demo/");
+                dir.mkdir();
+                File file=new File(dir, System.currentTimeMillis()+".jpg");
+                try {
+                    outputStream=new FileOutputStream(file);
+                }catch (FileNotFoundException e){
+                    e.printStackTrace();
+                }
+                bitmap.compress(Bitmap.CompressFormat.JPEG,100,outputStream);
+                Toast.makeText(getApplicationContext(),"Image Download to Internal !!!",Toast.LENGTH_SHORT).show();
+                try {
+                    outputStream.flush();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    outputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -110,8 +247,14 @@ public class scroll2 extends AppCompatActivity {
 
             return true;
         }
-          if (id == R.id.location) {
+        if (id == R.id.location) {
             Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://maps.app.goo.gl/QgfkFkSaarg1jbts8"));
+            startActivity(intent);
+
+            return true;
+        }
+        if (id == R.id.search) {
+            Intent intent = new Intent(scroll2.this,search.class);
             startActivity(intent);
 
             return true;
